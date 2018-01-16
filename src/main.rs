0747bf7402cb23badd28b76067f35c95df026f23
minamorl <minamorl@users.noreply.github.com>
2018-01-16 20:13:37 +0900

    Add code for JSON deserialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate serde_json;
+
+use std::fs::File;
+use serde_json::{Value, Error, Map};
+use std::io::{self, Read, BufRead, Write};
+
+fn parse_json(path: &str) -> Result<Map<String, Value>, Error> {
+    let mut file = File::open(path).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let result = serde_json::from_str(contents.as_str());
+    result
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let contents = parse_json("misc/dictionary.json").unwrap();
+    print!("> ");
+    io::stdout().flush().unwrap();
+
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    stdin.lock().read_line(&mut buffer).unwrap();
+
+    println!("{}", contents[buffer.as_str().trim()]);
 }
