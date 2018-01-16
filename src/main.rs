3c82ce4ca2b44e77cd1f4887b4663e5975955e0a
minamorl <minamorl@users.noreply.github.com>
2018-01-17 02:16:48 +0900

    Add InternalError to represent internal error kind

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,32 @@ extern crate serde_json;
 use std::fs::File;
 use serde_json::{Value, Error, Map};
 use std::io::{self, Read, BufRead, Write};
+use std::fmt;
 
-fn parse_json(path: &str) -> Result<Map<String, Value>, Error> {
-    let mut file = File::open(path).unwrap();
+
+#[derive(Debug)]
+enum InternalError {
+    DictionaryNotFound,
+    ParseFailed(Error)
+}
+
+impl fmt::Display for InternalError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            InternalError::DictionaryNotFound => "Dictionary not found".fmt(f),
+            InternalError::ParseFailed(ref e) => e.fmt(f)
+        }
+    }
+}
+
+fn parse_json(path: &str) -> Result<Map<String, Value>, InternalError> {
+    let mut file = File::open(path)
+        .map_err(|_| InternalError::DictionaryNotFound)?;
     let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    let result = serde_json::from_str(contents.as_str());
+    file.read_to_string(&mut contents)
+        .map_err(|_| InternalError::DictionaryNotFound)?;
+    let result = serde_json::from_str(contents.as_str())
+        .map_err(|e| InternalError::ParseFailed(e));
     result
 }
 
